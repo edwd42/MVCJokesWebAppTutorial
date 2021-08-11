f8669cc5f3c11841c9b708007c887a4d529fdaa9
edward.dupre <edward.dupre@redcross.org>
2021-08-11 18:08:53 -0400

    Add project files.

---
+++ MVCJokesWebAppTutorial/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCJokesWebAppTutorial.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
