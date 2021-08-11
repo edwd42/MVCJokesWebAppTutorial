9721b5580f83bde0767ecc1ac6d1e8e7b5a90da0
edward.dupre <edward.dupre@redcross.org>
2021-08-11 19:56:49 -0400

    add controller

--- MVCJokesWebAppTutorial/Data/ApplicationDbContext.cs
+++ MVCJokesWebAppTutorial/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MVCJokesWebAppTutorial.Models;
 
 namespace MVCJokesWebAppTutorial.Data
 {
@@ -12,5 +13,6 @@ namespace MVCJokesWebAppTutorial.Data
             : base(options)
         {
         }
+        public DbSet<MVCJokesWebAppTutorial.Models.Joke> Joke { get; set; }
     }
 }
